61b87aa677effbaa4007bf8fad574c9a94722038
AdrianT3 <166077113+t3knkk@users.noreply.github.com>
2025-10-01 09:44:10 -0300

    Update logger.py

--- projeto/logger.py
+++ projeto/logger.py
@@ -2,8 +2,6 @@ import logging
 from datetime import datetime
 
 def setup_logger():
-    """Configura o logger para registrar exceções em um arquivo."""
-    
     log_format = '%(asctime)s - %(levelname)s - %(message)s'
     
     logging.basicConfig(
@@ -20,4 +18,5 @@ def log_error(exception):
     Args:
         exception (Exception): A exceção que foi capturada.
     """
+
     logging.error(f"An exception occurred: {exception}", exc_info=True)
